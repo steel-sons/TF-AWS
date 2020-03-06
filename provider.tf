67a2c24314e2f097c0faacad805a30a527fa2840
istepanov <istepanov@exadel.com>
2020-03-06 23:29:09 +0200

    remote state updated version for CI/CD

--- provider.tf
+++ provider.tf
@@ -13,36 +13,36 @@ provider "aws" {
 ## Create S3 bucket #######
 ###########################
 
-#resource "aws_s3_bucket" "terraform_state" {
-#  bucket = "eks-cluster-s3-state"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "eks-cluster-s3-state"
   # Enable versioning so we can see the full revision history of our
   # state files
-#  versioning {
-#    enabled = true
-#  }
+  versioning {
+    enabled = true
+  }
   # Enable server-side encryption by default
-#  server_side_encryption_configuration {
-#    rule {
-#      apply_server_side_encryption_by_default {
-#        sse_algorithm = "AES256"
-#      }
-#    }
-#  }
-#}
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
 
 ######################################
 ## Creating DynamoDB for locking state
 ######################################
 
-#resource "aws_dynamodb_table" "terraform_locks" {
-#  name         = "eks-cluster-DynamoDB-tfstate"
-#  billing_mode = "PAY_PER_REQUEST"
-#  hash_key     = "LockID"
-#  attribute {
-#    name = "LockID"
-#    type = "S"
-#  }
-#}
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "eks-cluster-DynamoDB-tfstate"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 
 terraform {
   backend "s3" {
