2164fa500c0cbde69454b734ef6f1f88bcb2ce4c
istepanov <istepanov@exadel.com>
2020-03-06 15:27:53 +0200

    fixes for ASG

--- asg.tf
+++ asg.tf
@@ -1,23 +1,11 @@
-# moved this part into a separate file
-# EKS currently documents this required userdata for EKS worker nodes to
-# properly configure Kubernetes applications on the EC2 instance.
-# We utilize a Terraform local here to simplify Base64 encode this
-# information and write it into the AutoScaling Launch Configuration.
-# More information: https://docs.aws.amazon.com/eks/latest/userguide/launch-workers.html
-#locals {
-#  tf-eks-node-userdata = <<USERDATA
-#!/bin/bash
-#set -o xtrace
-#/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.eks_cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.eks_cluster.certificate_authority.0.data}' '${var.cluster_name}'
-#USERDATA
-#}
 ######################################
 #### AutoScaling launch configuration
 ######################################
+
 #### Public subnet
 resource "aws_launch_configuration" "tf_eks" {
   associate_public_ip_address = true
-  iam_instance_profile        = aws_iam_instance_profile.eks-node.name
+  iam_instance_profile        = aws_iam_instance_profile.node.name
   image_id                    = data.aws_ami.amazon-eks-linux-2.id # we getting the AMI from data source
   instance_type               = var.instance_type
   name_prefix                 = "terraform-eks-public"
@@ -53,10 +41,11 @@ resource "aws_autoscaling_group" "tf_eks_asg" {
   }
 }
 ###############################
-#### Private subnet
+#### Private subnet ###########
+###############################
 resource "aws_launch_configuration" "tf-eks-asg-private" {
   associate_public_ip_address = false
-  iam_instance_profile        = aws_iam_instance_profile.eks-node.name
+  iam_instance_profile        = aws_iam_instance_profile.node.name
   image_id                    = data.aws_ami.amazon-eks-linux-2.id # we getting the AMI from data source
   instance_type               = var.instance_type
   name_prefix                 = "terraform-eks-private"
